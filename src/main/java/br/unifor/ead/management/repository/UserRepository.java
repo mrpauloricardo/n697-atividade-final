b191e62c9fe7d878509cbeccd05a6873a793e706
LucasCamara13 <140122250+LucasCamara13@users.noreply.github.com>
2024-09-19 02:09:48 -0300

    adicionado o sistema de autenticação

--- src/main/java/br/unifor/ead/management/repository/UserRepository.java
+++ src/main/java/br/unifor/ead/management/repository/UserRepository.java
@@ -2,6 +2,9 @@ package br.unifor.ead.management.repository;
 
 import br.unifor.ead.management.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface UserRepository extends MongoRepository<User, String > {
+@RepositoryRestResource(path = "users", collectionResourceRel = "users")
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByEmail(String email);
 }
