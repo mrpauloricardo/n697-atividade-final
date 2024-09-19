d8782559d9e2bc07b545cf33b6edb190b3b9eb87
Rog3rm4in <145932345+Rog3rm4in@users.noreply.github.com>
2024-09-19 02:25:51 -0300

    adicionado entidade subject

--- src/main/java/br/unifor/ead/management/config/SecurityConfig.java
+++ src/main/java/br/unifor/ead/management/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,7 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())  // Desabilitar CSRF para simplificar o uso de APIs REST
+                .csrf(AbstractHttpConfigurer::disable)  // Desabilitar CSRF para simplificar o uso de APIs REST
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/register", "/login", "/public/**").permitAll()  // Permitir acesso público para registro e login
                         .anyRequest().authenticated()  // Todas as outras rotas requerem autenticação
